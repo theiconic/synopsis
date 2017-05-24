20516b579416a18845b9fab0324a339c2b7dfac3
Andre Wyrwa <andre.wyrwa@theiconic.com.au>
2017-05-24 13:45:02 +1000

    remove the @SuppressWarnings

--- src/AbstractSynopsis.php
+++ src/AbstractSynopsis.php
@@ -42,8 +42,6 @@ abstract class AbstractSynopsis
      *
      * @param $value
      * @param $depth
-     *
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function process($value, $depth)
     {
